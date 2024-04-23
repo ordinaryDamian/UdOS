2ed6639c02ac487ceb996db2ac4d1b0e2a76d897
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-04-23 19:17:44 +0000

    Update yayInstall.sh

--- yayInstall.sh
+++ yayInstall.sh
@@ -2,7 +2,7 @@
 
 echo "Installing yay (yet another yogurt) AUR helper."
 echo "What is AUR ? : https://itsfoss.com/aur-arch-linux/ "
-sleep 5
+sleep 15
 
 sudo pacman -S git base-devel --noconfirm
 git clone https://aur.archlinux.org/yay.git
