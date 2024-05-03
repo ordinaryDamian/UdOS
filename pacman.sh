5a892d965ee1137a937b6085a0eb75f80838fdee
ordinaryDamian <94764927+ordinaryDamian@users.noreply.github.com>
2024-05-03 21:59:31 +0200

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -12,7 +12,6 @@ sudo pacman -S pacman-contrib reflector --noconfirm
 echo "Rating mirrors"
 # sudo reflector --verbose --protocol https,http --latest 5 --sort rate --country Slovakia,Polland,Germany --save /etc/pacman.d/mirrorlist
 sudo reflector --verbose --protocol https,http,ftp --latest 100 --score 100 --sort rate --country SK,CZ,DE,HU,PL --fastest 10 --save /etc/pacman.d/mirrorlist
-sudo pacman -Syyu --noconfirm
 
 # Define the configuration
 config="--verbose
